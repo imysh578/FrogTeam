3c0fa666a45cd68d5f030c5883cb34cf2d104e1f
imysh578 <33863016+imysh578@users.noreply.github.com>
2021-11-09 03:22:52 +0900

    20211109 css 추가

--- src/components/ChartSection/Coingecko/index.jsx
+++ src/components/ChartSection/Coingecko/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import CoingeckoChart from './Chart'
+import CoinDetail from './CoinDetail'
 import CoingeckoCoinList from './CoinList'
 
 const Coingecko = () => {
   return (
     <>
-      <CoingeckoCoinList/>
+      <Routes>
+        <Route path="/" element={<CoingeckoCoinList/>} exact />
+        <Route path="/:id" element={<CoinDetail />} exact />
+      </Routes>
     </>
   )
 }
