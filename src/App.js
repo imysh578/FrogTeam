f5626023769cf63aaaa731f75c15540997eda88a
imysh578 <33863016+imysh578@users.noreply.github.com>
2021-11-12 17:42:22 +0900

    query page 추가

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import './App.css';
-
-
-import {BrowserRouter as Router, Routes, Route} from
- 'react-router-dom'
- import Home from './pages'
- import SigninPage from './pages/signin';
- import NewsPage from './pages/news';
-import CoinsPage from './pages/coins';
-import CoinDetail from './components/ChartSection/Coingecko/CoinDetail';
-
+import React from "react";
+import "./App.css";
+import QueryPage from './pages/query'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/signin";
+import NewsPage from "./pages/news";
+import CoinsPage from "./pages/coins";
+import CoinDetail from "./components/ChartSection/Coingecko/CoinDetail";
 
 function App() {
-  return (
-    <Router>
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} exact />
+				<Route path="signin" element={<SigninPage />} exact />
+				<Route path="news" element={<NewsPage />} exact />
+				<Route path="coins/*" element={<CoinsPage />} exact>
+					<Route path=":id" element={<CoinDetail />} />
+				</Route>
+				<Route path="query" element={<QueryPage />} exact />
 
-    <Routes>
-       <Route path="/" element={<Home />} exact />
-       <Route path="signin" element={<SigninPage />} exact />
-       <Route path="news" element={<NewsPage />} exact />
-       <Route path="coins/*" element={<CoinsPage />} exact>
-        <Route path=":id" element={<CoinDetail/>} />
-       </Route>
-    </Routes>
-        </Router>
-  );
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,16 +12,9 @@ import {
 	homeObjFive,
 } from "../components/InfoSection/Data";
 import Scroll from "../components/Scroll";
-import { QueryClientProvider, QueryClient } from "react-query";
-import Posts from "../components/ReactQuery/Posts";
-import Post from "../components/ReactQuery/Post";
-import Rest from "../components/ReactQuery/Rest";
 
-const queryClient = new QueryClient();
 
 const Home = () => {
-	const [postId, setPostId] = useState(-1);
-	const [업비트, 업비트변경] = useState(-1);
 	const [isOpen, setIsOpen] = useState(false);
 	const toggle = () => {
 		setIsOpen(!isOpen);
@@ -38,18 +31,6 @@ const Home = () => {
 			<InfoSection {...homeObjFive} />
 			<Scroll showBelow={250} />
 			<Footer />
-			{/* react-query 쓰려면 이거 주석 해제 해주세요 너무 더러워서 없애놈 ㅈㅅ */}
-			{/* <QueryClientProvider client={queryClient}>
-				<div>
-					react-query
-					<Rest />
-					{postId > -1 ? (
-						<Post postId={postId} setPostId={setPostId} />
-					) : (
-						<Posts setPostId={setPostId} />
-					)}
-				</div>
-			</QueryClientProvider> */}
 		</>
 	);
 };
