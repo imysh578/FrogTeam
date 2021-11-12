d750e608b246f1543271fec4377d54ce9ec11fa2
imysh578 <33863016+imysh578@users.noreply.github.com>
2021-11-12 17:37:13 +0900

    주석처리 조금 변경

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import InfoSection from "../components/InfoSection";
 import {
-  homeObjOne,
-  homeObjTwo,
-  homeObjThree,
-  homeObjFour,
-  homeObjFive,
+	homeObjOne,
+	homeObjTwo,
+	homeObjThree,
+	homeObjFour,
+	homeObjFive,
 } from "../components/InfoSection/Data";
 import Scroll from "../components/Scroll";
 import { QueryClientProvider, QueryClient } from "react-query";
@@ -20,37 +20,38 @@ import Rest from "../components/ReactQuery/Rest";
 const queryClient = new QueryClient();
 
 const Home = () => {
-  const [postId, setPostId] = useState(-1);
-  const [업비트, 업비트변경] = useState(-1);
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
-      <HeroSection />
-      <InfoSection {...homeObjOne} />
-      <InfoSection {...homeObjTwo} />
-      <InfoSection {...homeObjThree} />
-      <InfoSection {...homeObjFour} />
-      <InfoSection {...homeObjFive} />
-      <Scroll showBelow={250} />
-      <Footer />
-      {/* react-query 쓰려면 이거 주석 해제 해주세요 너무 더러워서 없애놈 ㅈㅅ */}
-      {/* <QueryClientProvider client={queryClient}> */}
-      {/* <div> */}
-      {/* react-query */}
-      {/* <Rest /> */}
-      {/* {postId > -1 ? ( */}
-      {/* // <Post postId={postId} setPostId={setPostId} /> */}
-      {/* // ) : ( // <Posts setPostId={setPostId} /> */}
-      {/* // )} */}
-      {/* </div> */}
-      {/* </QueryClientProvider> */}
-    </>
-  );
+	const [postId, setPostId] = useState(-1);
+	const [업비트, 업비트변경] = useState(-1);
+	const [isOpen, setIsOpen] = useState(false);
+	const toggle = () => {
+		setIsOpen(!isOpen);
+	};
+	return (
+		<>
+			<Sidebar isOpen={isOpen} toggle={toggle} />
+			<Navbar toggle={toggle} />
+			<HeroSection />
+			<InfoSection {...homeObjOne} />
+			<InfoSection {...homeObjTwo} />
+			<InfoSection {...homeObjThree} />
+			<InfoSection {...homeObjFour} />
+			<InfoSection {...homeObjFive} />
+			<Scroll showBelow={250} />
+			<Footer />
+			{/* react-query 쓰려면 이거 주석 해제 해주세요 너무 더러워서 없애놈 ㅈㅅ */}
+			{/* <QueryClientProvider client={queryClient}>
+				<div>
+					react-query
+					<Rest />
+					{postId > -1 ? (
+						<Post postId={postId} setPostId={setPostId} />
+					) : (
+						<Posts setPostId={setPostId} />
+					)}
+				</div>
+			</QueryClientProvider> */}
+		</>
+	);
 };
 
 export default Home;
