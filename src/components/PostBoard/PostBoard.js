3bbcc04d3bb13aa283b9d0f1a7b87403c1c269ed
TsukinoHikari <89628687+TsukinoHikari@users.noreply.github.com>
2021-11-18 04:28:44 +0900

    Update PostBoard.js

--- src/components/PostBoard/PostBoard.js
+++ src/components/PostBoard/PostBoard.js
@@ -1,10 +1,13 @@
+import React, { Component } from "react";
 import { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "./PostBoard.css";
 import ReactHtmlParser from "react-html-parser";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
+  //초기값
   const [areaContent, setAreaContent] = useState({
     nickname: "",
     content: "",
@@ -12,6 +15,7 @@ function App() {
 
   const [viewContent, setViewCintent] = useState([]);
 
+  //입력값
   const getValue = (e) => {
     const { name, value } = e.target;
     setAreaContent({
@@ -21,6 +25,58 @@ function App() {
     console.log(areaContent);
   };
 
+  // 날짜 출력하는법...?
+  // const Date = (d) => {
+  //   new this.props.date(this.props.toLocaleDateString);
+  //   console.log(d);
+  // };
+
+  //날짜및 시간
+  //윗쪽 하다가 안 먹어서 좀 고전적인 방법을 사용했습니다. 수정 가능성 有
+  const today = new Date();
+  const month = ("0" + (today.getMonth() + 1)).slice(-2);
+  const day = ("0" + today.getDate()).slice(-2);
+  const hour = ("0" + today.getHours()).slice(-2);
+  const minunt = ("0" + today.getMinutes()).slice(-2);
+
+  const dateString = month + "월 " + day + "일 " + hour + " : " + minunt;
+
+  //   //무한스크롤 설정값
+  //   class App extends React.Component {
+  //     state = {
+  //       items: Array.from({ length: 15 }),
+  //     };
+
+  //     fetchMoreData = () => {
+  //       // 1.5초 딜레이 후 5개의 내용 로드
+  //       setTimeout(() => {
+  //         this.setState({
+  //           items: this.state.items.concat(Array.from({ length: 5 })),
+  //         });
+  //       }, 1500);
+  //     };
+
+  //     //무한스크롤 렌더
+  //     render() {
+  //       return (
+  //         <div>
+  //           <hr />
+  //           <InfiniteScroll
+  //             dataLength={this.state.items.length}
+  //             next={this.fetchMoreData}
+  //             hasMore={true}
+  //             loader={<h4>Loading...</h4>}
+  //           >
+  //             {this.state.items.map((viewContent) => (
+  //               <div key={viewContent}>{viewContent}</div>
+  //             ))}
+  //           </InfiniteScroll>
+  //         </div>
+  //       );
+  //     }
+  //   }
+
+  //여기서부터 출력
   return (
     <div className="App">
       <h2 Style="text-align: center">ribbit-ribbit board</h2>
@@ -39,6 +95,8 @@ function App() {
       >
         RIBBIT
       </button>
+
+      {/* 포스트박스 */}
       <CKEditor
         editor={ClassicEditor}
         data="<p>내용...</p>"
@@ -62,7 +120,10 @@ function App() {
           console.log("Focus.", editor);
         }}
       />
+
+      {/* 작성글 출력 */}
       <div className="form-wrapper">
+        {/* <App> */}
         {viewContent.map((element) => (
           <div
             Style="
@@ -73,11 +134,14 @@ function App() {
             border-color: gray;"
           >
             <h4>{element.nickname}</h4>
+            {dateString}
+
             <div Style="margin-bottom : 50px">
               {ReactHtmlParser(element.content)}
             </div>
           </div>
         ))}
+        {/* </App> */}
       </div>
     </div>
   );
