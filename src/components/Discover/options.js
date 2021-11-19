34246ce09d646dadc872093027cda81b41fbf07e
Flavordash <you@example.com>
2021-11-19 15:56:00 +0900

    ㅏㅐㅏㅐ

---
+++ src/components/Discover/options.js
@@ -0,0 +1,11 @@
+const options = {
+  method: 'GET',
+  url: 'https://twelve-data1.p.rapidapi.com/etf',
+  params: {exchange: 'Euronext', format: 'json'},
+  headers: {
+    'x-rapidapi-host': 'twelve-data1.p.rapidapi.com',
+    'x-rapidapi-key': '19c6113a50msh1b0382cd6119651p166336jsn2094dddfaf2b'
+  }
+};
+
+export default options 
