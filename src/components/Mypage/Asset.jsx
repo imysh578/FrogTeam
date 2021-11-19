1b99dfa9a7725010974a6fc954bf4d6096d28f45
imysh578 <33863016+imysh578@users.noreply.github.com>
2021-11-20 03:39:27 +0900

    email 세션에서 받아오기 성공

--- src/components/Mypage/Asset.jsx
+++ src/components/Mypage/Asset.jsx
@@ -1,41 +1,42 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Button, ButtonGroup } from "react-bootstrap";
+import useAxios from "../../hooks/useAxios";
 
 const baseUrl = "http://localhost:5000";
 
 const Asset = ({ index, asset, editShow }) => {
 	const [inputMode, setInputMode] = useState(false);
-	const [buyPrice, setBuyPrice] = useState(0);
-	const [price, setPrice] = useState(0);
+	const [buyPrice, setBuyPrice] = useState(asset.avg_buy_price);
+	const [price, setPrice] = useState(asset.price);
 	const [totalBuyPrice, setTotalBuyPrice] = useState(0);
 	const [totalAsset, setTotalAsset] = useState(0);
-	const [balance, setBalance] = useState(0);
-	const [assetName, setAssetName] = useState(0);
-	const [exchange, setExchange] = useState(0);
+	const [balance, setBalance] = useState(asset.balance);
+	const [assetName, setAssetName] = useState(asset.currency);
+	const [exchange, setExchange] = useState(asset.exchange);
 	const [profit, setProfit] = useState(0);
 	const [profitRate, setProfitRate] = useState(0);
 
-	const [inputBuyPrice, setInputBuyPrice] = useState(asset.avg_buy_price);
-	const [inputBalance, setInputBalance] = useState(asset.balance);
+	const [inputBuyPrice, setInputBuyPrice] = useState(0);
+	const [inputBalance, setInputBalance] = useState(0);
+
+	// 로그인 정보 불러오기
+	const signIn = useAxios({
+    method: "get",
+    baseURL: "http://localhost:5000",
+    url: "auth/session",
+  });
 
 	// 버튼 핸들러
 	const handleEditClick = (e) => {
 		e.preventDefault();
 		setInputMode(!inputMode);
 	};
-	const handleSubmitClick = async (e) => {
+	const handleSubmitClick = (e) => {
 		e.preventDefault();
-		const result = await axios.post(baseUrl + "/assets/edit", {
-			email: "asdf@asdf",
-			exchange: exchange,
-			coinId: assetName,
-			amount: Number(balance),
-			buyPrice: Number(buyPrice),
-		});
-		setInputMode(!inputMode);
 		if (inputBuyPrice) setBuyPrice(inputBuyPrice);
 		if (inputBalance) setBalance(inputBalance);
+		setInputMode(!inputMode);
 	};
 	const handleCancleClick = (e) => {
 		e.preventDefault();
@@ -45,24 +46,22 @@ const Asset = ({ index, asset, editShow }) => {
 	// input 값 핸들러
 	const handleBuyPriceOnChange = (e) => {
 		setInputBuyPrice(e.target.value);
-		setBuyPrice(e.target.value);
 	};
 	const handleBalanceOnChange = (e) => {
 		setInputBalance(e.target.value);
-		setBalance(e.target.value);
 	};
 
 	useEffect(() => {
-		const exchange_temp = asset.exchange.toUpperCase();
-		const assetName_temp = asset.currency.toUpperCase();
-		const balance_temp = Number(Number(inputBalance).toFixed(2));
-		const price_temp = Number(Number(asset.price).toFixed(2));
-		const averageBuyPrice_temp = Number(Number(inputBuyPrice).toFixed(2));
+		const exchange_temp = exchange.toUpperCase();
+		const assetName_temp = assetName.toUpperCase();
+		const balance_temp = Number(Number(balance).toFixed(2));
+		const price_temp = Number(Number(price).toFixed(2));
+		const averageBuyPrice_temp = Number(Number(buyPrice).toFixed(2));
 		const totalAsset_temp = Number(
-			(Number(asset.price) * Number(inputBalance)).toFixed(0)
+			(Number(price) * Number(balance)).toFixed(0)
 		);
 		const totalBuyPrice_temp = Number(
-			Number(inputBuyPrice * Number(inputBalance)).toFixed(0)
+			Number(buyPrice * Number(balance)).toFixed(0)
 		);
 		const profit_temp = Number(
 			Number(totalAsset_temp) - Number(totalBuyPrice_temp)
@@ -73,7 +72,7 @@ const Asset = ({ index, asset, editShow }) => {
 				((profit_temp / totalBuyPrice_temp) * 100).toFixed(2)
 			);
 		}
-
+		
 		setPrice(price_temp);
 		setBuyPrice(averageBuyPrice_temp);
 		setTotalBuyPrice(totalBuyPrice_temp);
@@ -85,8 +84,18 @@ const Asset = ({ index, asset, editShow }) => {
 		setProfitRate(profitRate_temp);
 	}, [asset, inputMode]);
 
-	useEffect(() => {}, [inputMode]);
-
+	useEffect( async ()=>{
+		if(signIn){
+			const result = await axios.post(baseUrl + "/assets/edit", {
+				email: signIn.data.user.email,
+				exchange: exchange,
+				coinId: assetName,
+				amount: Number(balance),
+				buyPrice: Number(buyPrice),
+			});
+		}
+	}, [inputMode])
+	
 	return (
 		<>
 			<tr>
@@ -97,10 +106,11 @@ const Asset = ({ index, asset, editShow }) => {
 				<td>
 					<span>{price.toLocaleString()}원</span>
 				</td>
-				{inputMode ? (
+				{(editShow & inputMode) ? (
 					<>
 						<td>
 							<input
+								className="w-50"
 								type="number"
 								placeholder={buyPrice}
 								onChange={handleBuyPriceOnChange}
@@ -109,6 +119,7 @@ const Asset = ({ index, asset, editShow }) => {
 						</td>
 						<td>
 							<input
+								className="w-50"
 								type="number"
 								placeholder={balance}
 								onChange={handleBalanceOnChange}
@@ -136,15 +147,15 @@ const Asset = ({ index, asset, editShow }) => {
 					<td>
 						{inputMode ? (
 							<ButtonGroup>
-								<Button onClick={handleSubmitClick} variant="primary">
+								<Button onClick={handleSubmitClick} variant="primary" size="sm">
 									확인
 								</Button>
-								<Button onClick={handleCancleClick} variant="danger">
+								<Button onClick={handleCancleClick} variant="danger" size="sm">
 									취소
 								</Button>
 							</ButtonGroup>
 						) : (
-							<Button onClick={handleEditClick} variant="success">
+							<Button onClick={handleEditClick} variant="success" size="sm">
 								수정
 							</Button>
 						)}
